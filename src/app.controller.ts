d4a0578c91c157f3a04f9e0ba54196a8f7c9ccb7
Sárándi <sarandi.balint@gmail.com>
2022-12-14 12:24:24 +0100

    list macskak

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,32 @@
 import {
   Controller,
   Get,
+  Param,
+  Query,
   Render,
+  Post,
+  Body,
+  Redirect,
 } from '@nestjs/common';
+import { resourceLimits } from 'worker_threads';
+import { brotliDecompressSync } from 'zlib';
 import { AppService } from './app.service';
 import db from './db';
+import { CatDto } from './cat.dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  @Render('index')
-  index() {
-    return { message: 'Welcome to the homepage' };
+  @Render('list')
+  async listCats() {
+    const [rows] = await db.execute(
+      'SELECT szem_szin, suly FROM macskak ORDER BY suly DESC'
+    );
+
+    return {
+      macskak: rows,
+    };
   }
 }
