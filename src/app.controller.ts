45b2d736978c4fddef1e14c98ecb2be00152f3b6
Sárándi <sarandi.balint@gmail.com>
2022-12-14 13:21:59 +0100

    Kész feladat

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -20,13 +20,42 @@ export class AppController {
 
   @Get()
   @Render('list')
-  async listCats() {
-    const [rows] = await db.execute(
-      'SELECT szem_szin, suly FROM macskak ORDER BY suly DESC'
-    );
+  async index(@Query('szem_szin') szem_szin: string = '') {
+    if (szem_szin != "") {
+      const [rows] = await db.execute(
+        "SELECT szem_szin, suly FROM macskak WHERE szem_szin LIKE ?",
+        [szem_szin],
+      );
 
+      return {
+        macskak: rows,
+      };
+    } else {
+      const [rows] = await db.execute(
+        'SELECT szem_szin, suly FROM macskak ORDER BY suly DESC',
+      );
+
+      return {
+        macskak: rows,
+      };
+    }
+  }
+
+  @Get('cats/new')
+  @Render('form')
+  newCatForm() {
+    return {};
+  }
+
+  @Post('cats/new')
+  @Redirect()
+  async newCat(@Body() macska: CatDto) {
+    const [result]: any = await db.execute(
+      'INSERT INTO macskak (szem_szin, suly) VALUES (?, ?)',
+      [macska.szemszin, macska.suly],
+    );
     return {
-      macskak: rows,
+      url: '/',
     };
   }
 }
